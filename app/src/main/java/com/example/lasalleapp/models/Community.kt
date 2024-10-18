85621ca32d0370b028ca9e4edc92ef38e31476b6
Yahir Hernandez Delgado <jhd76934@udelasalle.edu.mx>
2024-10-18 00:35:00 -0600

    Recomposicion del proyecto

--- app/src/main/java/com/example/lasalleapp/models/Community.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lasalleapp.models
-
-data class Community(
-    val id: Int,
-    val image: String
-)
--- app/src/main/java/com/example/lasalleapp/models/Settings.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lasalleapp.models
-
-data class Settings(
-    val name : String,
-    val date : String,
-    val mail : String,
-    val image : String
-)
